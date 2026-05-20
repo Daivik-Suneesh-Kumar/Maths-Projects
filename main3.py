4065e9317b2d8aab82bbafab245cae6f9f60c27e
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-20 18:12:08 +0530

    Caluculating Percentage

---
+++ main3.py
@@ -0,0 +1,13 @@
+#Print
+print("Enter the marks of the student:")
+#Subjects
+Maths = int(input("Enter the marks:"))
+English = int(input("Enter the marks:"))
+Science = int(input("Enter the marks:"))
+Hindi = int(input("Enter the marks:"))
+#Sum
+sum = Maths+Science+English+Hindi
+print("The sum of marks of the student:")
+#Percentage
+perc = sum/4
+print("The percentage of the student is :",perc)
