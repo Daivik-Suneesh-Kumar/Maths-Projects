3b15f8ca183e5ce478a7e89366a4959d295cc229
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-20 17:50:46 +0530

    Average and Sum

---
+++ main.py
@@ -0,0 +1,13 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+
+#Sum
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of al  the trees are :", sum)
+
+#Average
+average = sum/5
+print("The average of trees are :", average)
