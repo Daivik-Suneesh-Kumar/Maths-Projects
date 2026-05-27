c0c5a44b526526bfe0bb83059fefdd70fe9ec4fe
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-27 17:14:01 +0530

    If and else

---
+++ main5.py
@@ -0,0 +1,10 @@
+#Declaring the variable
+
+num = int(input("Enter your number:"))
+
+#If and else commands
+
+if num >= 0:
+   print(num,"is a positive number.")
+else:
+   print(num,"is a negative number.")
