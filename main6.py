02de1bac484d55b3a102b70f544f1e446cfc16f2
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-27 17:27:30 +0530

    Profit and Loss

---
+++ main6.py
@@ -0,0 +1,12 @@
+#Declaring the variables
+original_amount = float(input("Enter the original amount:"))
+new_amount = float(input("Enter the new amount:"))
+
+#if and else
+
+if new_amount > original_amount:
+ amount = new_amount - original_amount
+ print("The profit is:",amount)
+else:
+ print("You have no profit!")
+
