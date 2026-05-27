532f4ffb9bc8acbde83caf405f34d9d6ef11c154
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-27 17:50:40 +0530

    temperature

---
+++ main9.py
@@ -0,0 +1,7 @@
+#Declaring the variable
+temp = int(input("How much is the temperature today?:"))
+#If and else statements
+if temp > 30:
+ print("It's really hot, don't go outside.")
+else:
+ print("It's cool, you can go outside.")
