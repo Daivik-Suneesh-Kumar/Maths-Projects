5ef74291cce3b681a235bf336ea9e8ba5f1be585
Daivik_Suneesh_Kumar <daivik.suneesh@gmail.com>
2026-05-27 17:43:50 +0530

    smaller or greater

---
+++ main8.py
@@ -0,0 +1,9 @@
+n = int(input("Enter your number:"))
+
+if n > 20:
+ print("n is bigger than 20")
+ print("i'm in if block.")
+else:
+ print("n is smaller than 20")
+ print("i'm in else block.")
+print("i'm not in if and else block.")
